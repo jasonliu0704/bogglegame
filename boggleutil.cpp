e12f7b790c192b2821a35dc42fdb17e3e298e0a9
Jiacheng Liu <jasonliu0704@gmail.com>
2015-12-01 23:39:02 -0800

    data structure

--- boggleutil.cpp
+++ boggleutil.cpp
@@ -1,7 +1,74 @@
+#include "boggleutil.h"
+#include <iostream>
+using namespace std;
 
 
-#include"boggleutil.h"
+bool WordTrie::search(string word) {
+  int i = 0;
+  TNode* cur = root; 
+  
+  while(1){
+    if(cur == 0 || i > word.length()){
+      return false;
+    }else if(cur->EOW && i == word.length()){
+      return true;
+    }else{
+      cur = (cur->children).at(word.at(i) - 'a');
+      i++;
+    }
+  }
+}
 
-//In this file, implement all of the operations your data structures support (you have declared these operations in boggleutil.h).
+TNode::TNode(char value, bool eow){
+  EOW = eow;
+  val = value;
+}
 
+bool MTrie::insert(string word) {
+  TNode* cur = root;
+  int i = 0;
 
+  while(1){
+
+    //check whether the node contains the letter exists
+    if((cur->children).at(word.at(i) - 'a') == 0){
+      //if not, then creat a new one for it
+      cur->childNum++;
+      (cur-children).at(word.at(i) - 'a') = new TNode(word.at(i), 
+      (i == word.size() - 1) ? true : false);
+    }
+    
+    //go check next letter
+    cur = cur = (cur-children).at(word.at(i) - 'a');
+    i++;
+
+    //check EOW
+    if(i == word.size()) return true;
+  }
+
+  return false;
+}
+
+MTrie::MTrie(){
+  root = new TNode(' ', false);
+}
+
+MTrie::~MTrie() {
+  clean(root);
+  delete root;
+  root = NULL;
+}
+
+MTrie::clean(TNode* n){
+  //base case, n's children array has 0 node
+  if(n->childNum == 0){
+    return;
+  }
+  
+  //loop through and clean all its children node
+  for(auto& node : n->children){
+    clean(node);
+    delete node;
+    node == 0;
+  }
+}
